cc244074e39ab1526918f06dede94920b43550ca
Patricio <patomolina88@gmail.com>
2020-09-15 17:23:43 -0300

    database connection

---
+++ DbConnectionBlog/ContextBlog.cs
@@ -0,0 +1,36 @@
+namespace DbConnectionBlog
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ContextBlog : DbContext
+    {
+        public ContextBlog()
+            : base("name=ContextBlog")
+        {
+        }
+
+        public virtual DbSet<blog_entrada> blog_entrada { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<blog_entrada>()
+                .Property(e => e.titulo)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<blog_entrada>()
+                .Property(e => e.contenido)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<blog_entrada>()
+                .Property(e => e.imagen)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<blog_entrada>()
+                .Property(e => e.categoria)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ DbConnectionBlog/blog_entrada.cs
@@ -0,0 +1,31 @@
+namespace DbConnectionBlog
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class blog_entrada
+    {
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string titulo { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string contenido { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string imagen { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string categoria { get; set; }
+
+        public DateTime fecha_creaciom { get; set; }
+    }
+}
