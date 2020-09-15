337863f2ae1702eedf16963d79803ae579a27cb7
Patricio <patomolina88@gmail.com>
2020-09-15 15:28:12 -0300

    add model

---
+++ blogPatricio/Modelo.Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blogPatricio
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public string imagen { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
